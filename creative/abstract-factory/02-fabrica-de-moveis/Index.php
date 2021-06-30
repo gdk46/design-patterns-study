ff7eaa471e83b6e0fc7014eb5ec6e38e43babf02
gdk46 <gleissonneves2017@gmail.com>
2021-06-30 08:10:23 -0300

    ex02

--- creative/abstract-factory/02-fabrica-de-moveis/Index.php
+++ creative/abstract-factory/02-fabrica-de-moveis/Index.php
@@ -2,3 +2,123 @@
 
 namespace Refactoring\AbstractFactory\Conceptual;
 
+interface RealEstateFactory
+{
+  public function createChair();
+  
+  public function createBed();
+}
+
+
+/**
+ * 
+ */
+class ModernFactory implements RealEstateFactory
+{
+  public function createChair()
+  {
+    return new ModernChair();
+  }
+  
+  public function createBed()
+  {
+    return new ModernBed();
+  }
+}
+
+
+/**
+ * 
+ */
+class VictorianFactory implements RealEstateFactory
+{
+  public function createChair()
+  {
+    return new VictorianChair();
+  }
+  
+  public function createBed()
+  {
+    return new VictorianBed();
+  }
+}
+
+
+interface ModernAbstract
+{
+  public function displayInformation(): string;
+}
+
+class ModernChair implements ModernAbstract
+{
+  public function displayInformation(): string
+  {
+    return "chair modern \n";
+  }
+}
+
+class ModernBed implements ModernAbstract
+{
+  public function displayInformation(): string
+  {
+    return "bed modern \n";
+  }
+}
+
+
+
+interface VictorianAbstract
+{
+  public function displayInformation(): string;
+}
+
+class VictorianChair implements VictorianAbstract
+{
+  public function displayInformation(): string
+  {
+    return "chair victorian \n";
+  }
+}
+
+class VictorianBed implements VictorianAbstract
+{
+  public function displayInformation(): string
+  {
+    return "bed victorian \n";
+  }
+}
+
+/**
+ * 
+ */
+class MakingFurniture
+{
+  private $modernFurniture;
+  private $victorianFurniture;
+ 
+  public function __construct()
+  {
+    $this->modernFurniture = new ModernFactory();
+    $this->victorianFurniture = new VictorianFactory();
+  }
+  
+  
+  public function modern()
+  {
+    return $this->modernFurniture->createChair()
+      ->displayInformation();
+  }
+  
+  public function victorian()
+  {
+    return $this->victorianFurniture->createChair()
+      ->displayInformation();
+  }
+}
+
+
+$factory = new MakingFurniture();
+
+echo "<pre>";
+echo $factory->modern();
+echo $factory->victorian();
